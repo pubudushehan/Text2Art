5f09530a99efc384f5081e1c803011a3cafa8d26
Pubudu_Shehan <110683869+pubudushehan@users.noreply.github.com>
2025-01-29 12:49:01 +0530

    Set Rutes

--- back-end/routes/StabilityAIRoutes.js
+++ back-end/routes/StabilityAIRoutes.js
@@ -7,7 +7,7 @@ dotenv.config();
 const router = express.Router();
 
 router.route("/").get((req, res) => {
-  res.status(200).json({ message: "Hello from DALL-E!" });
+  res.status(200).json({ message: "Hello from Stability AI!" });
 });
 
 router.route("/").post(async (req, res) => {
